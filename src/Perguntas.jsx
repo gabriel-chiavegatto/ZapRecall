c73f64e145a81af00435631045019b0097520c54
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-03-18 20:56:51 -0300

    refatoraçao css

--- src/Perguntas.jsx
+++ src/Perguntas.jsx
@@ -1,9 +1,28 @@
-export default function Perguntas() {
-    return (
-        <section className="caixa-pergunta">
-            <p>Pergunta aqui</p>
-            <img className="play" src="./img/Vector.png" alt="logo"></img>
-        </section>
+import React from "react";
+export default function Perguntas(props) {
+    const { pergunta } = props;
+    const [stage, setStage] = React.useState("stage1")
 
+    function virarPergunta() {
+        setStage("stage2");
+        verificarStage();
+    }
+    function verificarStage() {
+        if (stage === "stage1") {
+
+        } if (stage === "stage2") {
+
+        } if (stage === "stage2") {
+
+        }
+
+    }
+    return (
+        <section className="caixa-perguntas">
+            <div className="perguntasStage1">
+                <p>{pergunta}</p>
+                <img onClick={virarPergunta} className="play" src="./img/Vector.png" alt="logo"></img>
+            </div>
+        </section>
     )
 }
