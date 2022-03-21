65eff380a33db4e96ead23e7d068e817ec12af10
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-03-21 03:51:27 -0300

    perguntas ok

--- src/componentes/Perguntas.jsx
+++ src/componentes/Perguntas.jsx
@@ -1,39 +1,52 @@
 import React from "react";
 export default function Perguntas(props) {
 
+    let desempenho;
     const { ordinario, pergunta, resposta } = props;
     const [stage, setStage] = React.useState("stage1");
+    const [contStage1, setContStage1] = React.useState("conteudo-stage1");
+    const [contStage2, setContStage2] = React.useState("conteudo-stage2 escondido");
+    const [contStage3, setContStage3] = React.useState("conteudo-stage3 escondido");
+    const [contStage4, setContStage4] = React.useState("conteudo-stage4 escondido");
 
-    function mudarStage() {
-        if (stage === "stage1") {
-            setStage("stage2");
-        } if (stage === "stage2") {
-            setStage("stage3");
-        } if (stage === "stage3") {
-            setStage("stage4");
-        }
+    function abrirPergunta() {
+        setStage("stage2");
+        setContStage1("conteudo-stage1 escondido");
+        setContStage2("conteudo-stage2");
+    }
+    function mostrarResposta() {
+        setStage("stage3");
+        setContStage2("conteudo-stage2 escondido");
+        setContStage3("conteudo-stage3");
+    }
+    function gravarDesempenho(imagem) {
+        setStage("stage4");
+        setContStage3("conteudo-stage3 escondido");
+        setContStage4("conteudo-stage4");
+        desempenho = imagem;
     }
 
     return (
         <section className={stage}>
-            <div className="conteudo-stage1">
+            <div className={contStage1}>
                 <p className="ordem" >{ordinario}</p>
-                <img onClick={mudarStage} className="play" src="./img/Vector.png" alt="logo"></img>
+                <img onClick={abrirPergunta} className="play" src="./img/Vector.png" alt="logo"></img>
             </div>
-            <div className="conteudo-stage2">
+            <div className={contStage2}>
                 <p> {pergunta} </p>
+                <img onClick={mostrarResposta} className="virar-a-resposta" src="./img/virar.png" alt="virar" />
             </div>
-            <div className="conteudo-stage3">
+            <div className={contStage3}>
                 <p> {resposta} </p>
                 <div className="pontuacao">
-                    <button className="ruim">Não lembrei</button>
-                    <button className="medio">Quase não lembrei</button>
-                    <button className="bom">Zap!</button>
+                    <button onClick={() => gravarDesempenho("'./img/acertou.png'")} className="ruim">Não lembrei</button>
+                    <button onClick={() => gravarDesempenho("'./img/acertou.png'")} className="medio">Quase não lembrei</button>
+                    <button onClick={() => gravarDesempenho("'./img/acertou.png'")} className="bom">Zap!</button>
                 </div>
             </div>
-            <div className="conteudo-stage4">
+            <div className={contStage4}>
                 <p>{ordinario}</p>
-                {/* <img src="" alt="Sua pontuação" /> */}
+                <img src={desempenho} alt="Seu desempenho" />
             </div>
 
         </section>
