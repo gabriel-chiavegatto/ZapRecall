cd21999c1eb90bfbe52501044302b67023c80aff
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-05-16 16:38:33 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom"
 import Iniciar from "./componentes/Iniciar"
-
+import './assets/css/reset.css';
+import './assets/css/style.css';
 function App() {
     return (
             <Iniciar />
